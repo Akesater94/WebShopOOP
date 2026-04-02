dd9ea5095cbc9d772d0a5f298a914a6c8acfdf96
omark96 <43138339+omark96@users.noreply.github.com>
2026-04-02 09:42:54 +0200

    Added a page header

---
+++ RajoSpritButik/RajoSpritButik/App.cs
@@ -0,0 +1,26 @@
+﻿namespace RajoSpritButik;
+
+internal class App
+{
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Clear();
+            DrawHeader();
+
+            Console.ReadKey();
+        }
+    }
+
+    public void DrawHeader()
+    {
+
+        List<string> listItems = new() { "Rajos Spritbutik", "Kunglig hovleverantör" };
+
+        string longestItem = listItems.OrderBy(s => s.Length).First();
+
+        Window window = new Window("", (Console.WindowWidth - longestItem.Length) / 2 - 4, 0, listItems);
+        window.Draw();
+    }
+}
