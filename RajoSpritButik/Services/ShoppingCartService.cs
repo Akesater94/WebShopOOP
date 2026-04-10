64512a0b59098e32fcb791a14d2ee395bdcd77db
omark96 <43138339+omark96@users.noreply.github.com>
2026-04-10 15:55:19 +0200

    Clean up comments in AddRowAsync method
    Removed commented-out code for clarity.

--- RajoSpritButik/Services/ShoppingCartService.cs
+++ RajoSpritButik/Services/ShoppingCartService.cs
@@ -7,10 +7,6 @@ public class ShoppingCartService(IShoppingCartRepository shoppingCartRepository)
 {
     public async Task AddRowAsync(int shoppingCartId, int productId)
     {
-        // Hämta raden från repot
-        // Finns inte raden, skapa ny rad
-        // Finns raden, lägg till ett på kvantitet
-        // await shoppingCartRepository.AddRowAsync(row)
         await shoppingCartRepository.AddRowAsync(shoppingCartId, productId);
     }
 
