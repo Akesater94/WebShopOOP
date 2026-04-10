97dbcbd5469c3c1fbd809f24eea0c4f6f9e75c6f
Akesater94 <aron.akesater@gmail.com>
2026-04-10 20:12:06 +0200

    Added function to display and choose shippingalternative.

---
+++ RajoSpritButik/Services/Interfaces/IShippingAlternativeService.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Interfaces;
+
+public interface IShippingAlternativeService
+{
+    Task<List<ShippingAlternative>> GetAllShippingAlternativesAsync();
+}
---
+++ RajoSpritButik/Services/ShippingAlternativeService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services;
+
+public class ShippingAlternativeService(IShippingAlternativeRepository shippingAlternativeRepository) : IShippingAlternativeService
+{
+    public async Task<List<ShippingAlternative>> GetAllShippingAlternativesAsync()
+    {
+        return await shippingAlternativeRepository.GetAllShippingAlternativesAsync();
+    }
+}
