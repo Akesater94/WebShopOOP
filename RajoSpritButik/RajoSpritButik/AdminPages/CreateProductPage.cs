45e8abfec6bfb310c6a730f8312377dcc694fde7
omark96 <43138339+omark96@users.noreply.github.com>
2026-04-13 09:13:34 +0200

    Fix typo

--- RajoSpritButik/RajoSpritButik/AdminPages/CreateProductPage.cs
+++ RajoSpritButik/RajoSpritButik/AdminPages/CreateProductPage.cs
@@ -39,8 +39,8 @@ internal class CreateProductPage : Page
             case 3: Console.Write("3. Skriv in lagersaldo: "); break;
             case 4: Console.Write("4. Skriv in ID för tillverkaren: "); break;
             case 5: Console.Write("5. Skriv in ID för kategorin: "); break;
-            case 6: Console.Write("5. Skriv en beskrivning: "); break;
-            case 7: Console.Write("6. Visa varan på startsidan J/N: "); break;
+            case 6: Console.Write("6. Skriv en beskrivning: "); break;
+            case 7: Console.Write("7. Visa varan på startsidan J/N: "); break;
             default: Console.WriteLine("Sparar produkt..."); break;
         }
     }
